14a877a6c20026c976899dbb7bd466dfee3a2f46
bmarvinb <mdmitry94@gmail.com>
2023-07-05 23:46:19 +0300

    fix: remove handlers from contact

--- src/test/mocks/handlers/api/contact.ts
+++ src/test/mocks/handlers/api/contact.ts
@@ -1,26 +1,7 @@
 import { rest } from "msw";
-import { ContactDTO } from "@/app/contact/services/contact";
 
 export const contactHandlers = [
-  rest.post(`/api/contact`, async (req, res, ctx) => {
-    const data: ContactDTO = await req.json();
-
-    if (!data.email.includes("@")) {
-      return res(
-        ctx.delay(300),
-        ctx.status(400),
-        ctx.json({ message: "Invalid email" })
-      );
-    }
-
-    if (data.question.includes("Error")) {
-      return res(
-        ctx.delay(300),
-        ctx.status(500),
-        ctx.json({ message: "Server error" })
-      );
-    }
-
+  rest.post(`/api/contact`, async (_req, res, ctx) => {
     return res(ctx.delay(400), ctx.status(200), ctx.json({ healthy: true }));
   }),
 ];
