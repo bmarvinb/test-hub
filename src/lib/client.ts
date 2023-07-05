f04e4ad2f0ab4074d1334b03db6c6dc5b11242ca
bmarvinb <mdmitry94@gmail.com>
2023-07-05 23:39:41 +0300

    refactor: client exports

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,11 +1,11 @@
-export interface ApiClient {
+interface ApiClient {
   get: <T = unknown>(endpoint: string) => Promise<T>;
   post: <T = void>(endpoint: string, body: BodyInit) => Promise<T>;
   patch: <T = void>(endpoint: string, body: BodyInit) => Promise<T>;
   delete: <T = void>(endpoint: string) => Promise<T>;
 }
 
-export const client: ApiClient = {
+const client: ApiClient = {
   get: (endpoint) => fetcher(endpoint, { method: "GET" }),
   post: (endpoint, body) => fetcher(endpoint, { body, method: "POST" }),
   patch: (endpoint, body) => fetcher(endpoint, { body, method: "PATCH" }),
@@ -37,3 +37,6 @@ async function fetcher<T = unknown>(
   }
   return await response.json();
 }
+
+export type { ApiClient };
+export { client };
