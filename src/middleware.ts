13e5fc7111494510cada72079411542182bd3b67
bmarvinb <mdmitry94@gmail.com>
2023-07-08 19:46:10 +0300

    test: remove all tests

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,45 +1,48 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import acceptLanguage from "accept-language";
-import { fallbackLng, languages } from "./app/i18n/settings";
+import { languages } from "./app/i18n/settings";
 
 acceptLanguage.languages(languages);
 
 export const config = {
-  matcher: "/:lng*",
+  matcher: ["/((?!api|_next/static|_next/image|assets|favicon.ico|sw.js).*)"],
 };
 
-const cookieName = "i18next";
+// const cookieName = "i18next";
 
-export function middleware(request: NextRequest) {
-  let lng;
-  if (request.cookies.has(cookieName))
-    lng = acceptLanguage.get(
-      request.cookies.get(cookieName)
-        ? request.cookies.get(cookieName)?.value
-        : fallbackLng
-    );
-  if (!lng) lng = acceptLanguage.get(request.headers.get("Accept-Language"));
-  if (!lng) lng = fallbackLng;
+export function middleware() {
+  // TODO: https://locize.com/blog/next-13-app-dir-i18n/
+  // request: NextRequest
 
-  // Redirect if lng in path is not supported
-  if (
-    !languages.some((loc) => request.nextUrl.pathname.startsWith(`/${loc}`)) &&
-    !request.nextUrl.pathname.startsWith("/_next")
-  ) {
-    return NextResponse.redirect(
-      new URL(`/${lng}${request.nextUrl.pathname}`, request.url)
-    );
-  }
+  // let lng;
+  // if (request.cookies.has(cookieName))
+  //   lng = acceptLanguage.get(
+  //     request.cookies.get(cookieName)
+  //       ? request.cookies.get(cookieName)?.value
+  //       : fallbackLng
+  //   );
+  // if (!lng) lng = acceptLanguage.get(request.headers.get("Accept-Language"));
+  // if (!lng) lng = fallbackLng;
+  // console.log("lng", lng);
 
-  if (request.headers.has("referer")) {
-    const refererUrl = new URL(request.headers.get("referer") || "");
-    const lngInReferer = languages.find((l) =>
-      refererUrl.pathname.startsWith(`/${l}`)
-    );
-    const response = NextResponse.next();
-    if (lngInReferer) response.cookies.set(cookieName, lngInReferer);
-    return response;
-  }
+  // if (
+  //   !languages.some((loc) => request.nextUrl.pathname.startsWith(`/${loc}`)) &&
+  //   !request.nextUrl.pathname.startsWith("/_next")
+  // ) {
+  //   return NextResponse.redirect(
+  //     new URL(`/${lng}${request.nextUrl.pathname}`, request.url)
+  //   );
+  // }
+
+  // if (request.headers.has("referer")) {
+  //   const refererUrl = new URL(request.headers.get("referer") || "");
+  //   const lngInReferer = languages.find((l) =>
+  //     refererUrl.pathname.startsWith(`/${l}`)
+  //   );
+  //   const response = NextResponse.next();
+  //   if (lngInReferer) response.cookies.set(cookieName, lngInReferer);
+  //   return response;
+  // }
 
   return NextResponse.next();
 }
