10932b8e40f6a12247f43b60cf161c6a3b7f74e9
bmarvinb <mdmitry94@gmail.com>
2023-07-08 20:32:27 +0300

    refactor: title component

--- src/components/ui/Title.tsx
+++ src/components/ui/Title.tsx
@@ -18,16 +18,31 @@ const titleVariants = cva("font-bold text-gray-800", {
 });
 
 export interface TitleProps
-  extends React.HTMLAttributes<HTMLTitleElement>,
+  extends React.HTMLAttributes<HTMLElement>,
     VariantProps<typeof titleVariants> {}
 
 export const Title = (props: TitleProps) => {
   const size = props.size || "h1";
-  const HeadingTag = size;
 
-  return (
-    <HeadingTag className={cn(titleVariants({ className: props.className }))}>
-      {props.children}
-    </HeadingTag>
-  );
+  const attrs = {
+    className: cn(titleVariants({ className: props.className })),
+    ...props,
+  };
+
+  switch (size) {
+    case "h1":
+      return <h1 {...attrs}>{props.children}</h1>;
+    case "h2":
+      return <h2 {...attrs}>{props.children}</h2>;
+    case "h3":
+      return <h3 {...attrs}>{props.children}</h3>;
+    case "h4":
+      return <h4 {...attrs}>{props.children}</h4>;
+    case "h5":
+      return <h5 {...attrs}>{props.children}</h5>;
+    case "h6":
+      return <h6 {...attrs}>{props.children}</h6>;
+    default:
+      break;
+  }
 };
