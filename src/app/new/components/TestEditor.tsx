68894f63614cb346dfb4ef5ac3c5723c18bea4a6
bmarvinb <mdmitry94@gmail.com>
2023-07-09 10:03:09 +0300

    feat: basic test editor UI

--- src/app/new/components/TestEditor.tsx
+++ src/app/new/components/TestEditor.tsx
@@ -1,3 +1,80 @@
-export const TestEditor = () => {
-  return null;
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import { Button } from "@/components/ui/Button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/Form";
+import { Input } from "@/components/ui/Input";
+import { useForm } from "react-hook-form";
+import { Textarea } from "@/components/ui/Textarea";
+
+export type TestEditorModel = z.infer<typeof formSchema>;
+
+export interface TestEditorProps {
+  onSubmit: (data: TestEditorModel) => void;
+}
+
+const formSchema = z.object({
+  title: z.string(),
+  description: z.string(),
+});
+
+export const TestEditor = (props: TestEditorProps) => {
+  const form = useForm<TestEditorModel>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      description: "",
+    },
+  });
+
+  const onSubmit = (values: TestEditorModel) => {
+    props.onSubmit({
+      title: values.title,
+      description: values.description,
+    });
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Title</FormLabel>
+              <FormControl>
+                <Input placeholder="Title" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="description"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Description</FormLabel>
+              <FormControl>
+                <Textarea placeholder="Description" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button type="submit">Create</Button>
+      </form>
+    </Form>
+  );
 };
--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,4 +1,5 @@
-import { Button } from "@/components/ui/Button";
+"use client";
+
 import { Title } from "@/components/ui/Title";
 import { TestEditor } from "./components/TestEditor";
 
@@ -7,11 +8,7 @@ export default function NewTest() {
     <div className="max-w-4xl mx-auto py-8 px-6">
       <Title>New test</Title>
 
-      <TestEditor />
-
-      <div className="flex mb-8">
-        <Button>Create</Button>
-      </div>
+      <TestEditor onSubmit={(data) => console.log("data", data)} />
     </div>
   );
 }
