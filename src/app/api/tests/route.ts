33df5f12726220b2f34e19910136ba4d3a25fd90
bmarvinb <mdmitry94@gmail.com>
2023-07-12 22:39:23 +0300

    build: attempt to fix broken build #5

--- src/app/api/tests/route.ts
+++ src/app/api/tests/route.ts
@@ -1,38 +1,38 @@
-import { NextResponse } from "next/server";
-import { createTestSchema, testsService } from "./services/tests-service";
+// import { NextResponse } from "next/server";
+// import { createTestSchema, testsService } from "./services/tests-service";
 
-export async function POST(req: Request) {
-  const data = JSON.parse(await req.json());
-  const response = createTestSchema.safeParse(data);
+// export async function POST(req: Request) {
+//   const data = JSON.parse(await req.json());
+//   const response = createTestSchema.safeParse(data);
 
-  if (!response.success) {
-    return NextResponse.json(
-      {
-        message: "Invalid request parameters",
-        issues: response.error.issues,
-      },
-      {
-        status: 400,
-      }
-    );
-  }
+//   if (!response.success) {
+//     return NextResponse.json(
+//       {
+//         message: "Invalid request parameters",
+//         issues: response.error.issues,
+//       },
+//       {
+//         status: 400,
+//       }
+//     );
+//   }
 
-  try {
-    const { id } = await testsService.create(response.data);
-    return NextResponse.json(
-      { message: "Test created", id },
-      {
-        status: 200,
-      }
-    );
-  } catch (error) {
-    return NextResponse.json(
-      {
-        message: "Error while creating test",
-      },
-      {
-        status: 400,
-      }
-    );
-  }
-}
+//   try {
+//     const { id } = await testsService.create(response.data);
+//     return NextResponse.json(
+//       { message: "Test created", id },
+//       {
+//         status: 200,
+//       }
+//     );
+//   } catch (error) {
+//     return NextResponse.json(
+//       {
+//         message: "Error while creating test",
+//       },
+//       {
+//         status: 400,
+//       }
+//     );
+//   }
+// }
--- src/app/api/tests/services/tests-service.ts
+++ src/app/api/tests/services/tests-service.ts
@@ -1,72 +1,72 @@
-import { z } from "zod";
+// import { z } from "zod";
 
-export enum QuestionType {
-  SingleChoice = "single-choice",
-  MultipleChoice = "multiple-choice",
-  NumberInput = "number-input",
-  TextInput = "text-input",
-}
+// export enum QuestionType {
+//   SingleChoice = "single-choice",
+//   MultipleChoice = "multiple-choice",
+//   NumberInput = "number-input",
+//   TextInput = "text-input",
+// }
 
-const QuestionChoiceOptionSchema = z.object({
-  value: z.string(),
-  isAnswer: z.boolean(),
-});
+// const QuestionChoiceOptionSchema = z.object({
+//   value: z.string(),
+//   isAnswer: z.boolean(),
+// });
 
-const choiceBasedQuestionSchema = z.object({
-  type: z.enum([QuestionType.SingleChoice, QuestionType.MultipleChoice]),
-  question: z.string(),
-  options: z.array(QuestionChoiceOptionSchema),
-});
+// const choiceBasedQuestionSchema = z.object({
+//   type: z.enum([QuestionType.SingleChoice, QuestionType.MultipleChoice]),
+//   question: z.string(),
+//   options: z.array(QuestionChoiceOptionSchema),
+// });
 
-const numberInputQuestionSchema = z.object({
-  type: z.literal(QuestionType.NumberInput),
-  question: z.string(),
-  answer: z.number(),
-  tolerance: z.number(),
-});
+// const numberInputQuestionSchema = z.object({
+//   type: z.literal(QuestionType.NumberInput),
+//   question: z.string(),
+//   answer: z.number(),
+//   tolerance: z.number(),
+// });
 
-const textInputQuestionSchema = z.object({
-  type: z.literal(QuestionType.TextInput),
-  question: z.string(),
-  answer: z.union([z.string(), z.array(z.string())]),
-});
+// const textInputQuestionSchema = z.object({
+//   type: z.literal(QuestionType.TextInput),
+//   question: z.string(),
+//   answer: z.union([z.string(), z.array(z.string())]),
+// });
 
-const testQuestionSchema = z.union([
-  choiceBasedQuestionSchema,
-  numberInputQuestionSchema,
-  textInputQuestionSchema,
-]);
+// const testQuestionSchema = z.union([
+//   choiceBasedQuestionSchema,
+//   numberInputQuestionSchema,
+//   textInputQuestionSchema,
+// ]);
 
-export const createTestSchema = z.object({
-  title: z.string(),
-  description: z.string(),
-  questions: z.array(testQuestionSchema),
-});
+// export const createTestSchema = z.object({
+//   title: z.string(),
+//   description: z.string(),
+//   questions: z.array(testQuestionSchema),
+// });
 
-export type CreateTestDTO = z.infer<typeof createTestSchema>;
+// export type CreateTestDTO = z.infer<typeof createTestSchema>;
 
-import prisma from "@/lib/prisma";
+// import prisma from "@/lib/prisma";
 
-export interface TestsService {
-  create(test: CreateTestDTO): Promise<{ id: string }>;
-}
+// export interface TestsService {
+//   create(test: CreateTestDTO): Promise<{ id: string }>;
+// }
 
-export const testsService: TestsService = {
-  create: async (testDTO: CreateTestDTO) => {
-    // const test = new TestModel(testDTO);
+// export const testsService: TestsService = {
+//   create: async (testDTO: CreateTestDTO) => {
+//     // const test = new TestModel(testDTO);
 
-    try {
-      const { id } = await prisma.test.create({
-        data: {
-          title: testDTO.title,
-          description: testDTO.description,
-        },
-      });
+//     try {
+//       const { id } = await prisma.test.create({
+//         data: {
+//           title: testDTO.title,
+//           description: testDTO.description,
+//         },
+//       });
 
-      return { id };
-    } catch (error) {
-      console.error("Test creation failed", error);
-      throw error;
-    }
-  },
-};
+//       return { id };
+//     } catch (error) {
+//       console.error("Test creation failed", error);
+//       throw error;
+//     }
+//   },
+// };
