9f3596ce964ad986d0fd0a5ce0fcd2375b9e31eb
bmarvinb <mdmitry94@gmail.com>
2023-07-12 22:28:11 +0300

    build: attempt to fix broken build

--- src/app/api/tests/services/tests-service.ts
+++ src/app/api/tests/services/tests-service.ts
@@ -1,5 +1,6 @@
 import { CreateTestDTO } from "@/shared/dtos/test-dto";
-import { TestModel } from "../models/test-model";
+// import { TestModel } from "../models/test-model";
+import prisma from "@/lib/prisma";
 
 export interface TestsService {
   create(test: CreateTestDTO): Promise<{ id: string }>;
@@ -7,10 +8,15 @@ export interface TestsService {
 
 export const testsService: TestsService = {
   create: async (testDTO: CreateTestDTO) => {
-    const test = new TestModel(testDTO);
+    // const test = new TestModel(testDTO);
 
     try {
-      const { id } = await test.create();
+      const { id } = await prisma.test.create({
+        data: {
+          title: testDTO.title,
+          description: testDTO.description,
+        },
+      });
 
       return { id };
     } catch (error) {
