d34cdefae101a09afc95e9f8c054f21df67eba51
bmarvinb <mdmitry94@gmail.com>
2023-07-14 17:14:18 +0300

    feat: view test page

---
+++ src/app/tests/[id]/page.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { testsService } from "@/app/new/services/tests-service";
+import { Title } from "@/components/ui/Title";
+import { Test } from "@/shared/models/test-model";
+import { useQuery } from "@tanstack/react-query";
+
+export default function ViewTest(data: { params: { id: string } }) {
+  const id = data.params.id;
+
+  const test = useQuery<unknown, { message: string }, Test>(
+    ["test", id],
+    () => {
+      return testsService.find(id);
+    }
+  );
+
+  return (
+    <div className="max-w-4xl mx-auto py-8 px-6">
+      <Title className="mb-8">View test</Title>
+
+      {test.isLoading ? (
+        <div>Loading...</div>
+      ) : test.isSuccess ? (
+        <div>
+          <div>Title: {test.data.title}</div>
+          <div>Description: {test.data.description}</div>
+        </div>
+      ) : (
+        <div>Error</div>
+      )}
+    </div>
+  );
+}
